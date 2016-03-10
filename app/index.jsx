9eb30e8077d2182c1e4fa04dfb45cb294ba02430
Emily Bullard <Emilygracebullard@gmail.com>
2016-03-09 20:26:37 -0500

    initial commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,3 +1,46 @@
 import style from './styles/';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('now running!')
+//WORK IN PROGRESS:
+
+class HelloMessage extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      message : ['hello', 'world']
+    };
+  }
+
+  _clickHandler(event) {
+    console.log(this.refs.message.value);
+    this.setState ({
+      message : event.target.value
+    });
+    console.log(this.state.message);
+  }
+
+  _handleButton(event) {
+    var newMessages = this.state.messageList.slice();
+    newMessages.push(this.state.message)
+    this.setState({
+      message: newMessages
+    })
+    console.log(this.state.message);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref="message" value={this.state.value} onChange="" />
+        <button onClick={this._clickHandler.bind(this)}>Click Me</button>
+        <ul>{}</ul>
+      </div>
+    );
+  }
+}
+
+
+var mountPoint = document.querySelector("#app");
+ReactDOM.render(<HelloMessage/>, mountPoint);
